c67b0a1a5e9cd90a1916bc6240db48498c1dca73
prasanna <prasanna1@gmail.com>
2025-12-11 22:57:45 +0530

    Initial setup done

---
+++ Modular Coding project/components/footer.js
@@ -0,0 +1,7 @@
+export function footer() {
+  return `
+    <footer style="padding:2px; background:lightblue; color:black; margin-top:30px;">
+      <p>Thanks for visiting!</p>
+    </footer>
+  `;
+}
---
+++ Modular Coding project/scripts/todos.js
@@ -0,0 +1,16 @@
+import { displayTodos } from "./displayTodos.js";
+
+let isLoggedIn = localStorage.getItem("isLoggedIn");
+
+if (!isLoggedIn) {
+  alert("Please login first");
+  window.location.href = "login.html";
+}
+
+async function fetchTodos() {
+  let res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  let data = await res.json();
+  displayTodos(data);
+}
+
+fetchTodos();

e259ed970d7c1b0116a6c20f60e224e262d87b14
saiprasanna <saiprasannavelakurthi@gmail.com>
2025-12-11 23:02:42 +0530

    Delete Modular Coding project directory

--- Modular Coding project/components/footer.js
+++
@@ -1,7 +0,0 @@
-export function footer() {
-  return `
-    <footer style="padding:2px; background:lightblue; color:black; margin-top:30px;">
-      <p>Thanks for visiting!</p>
-    </footer>
-  `;
-}
